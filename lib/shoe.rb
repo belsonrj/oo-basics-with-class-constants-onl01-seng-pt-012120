c59a1dd828089845b2327cc584885ef3d9ff5bec
Rick Belson <belsonrj@aol.com>
2020-02-08 19:38:56 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,6 @@
 class Shoe
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+  attr_accessor :color, :size, :material, :condition, :brand
+  attr_reader 
   
   BRANDS = [] 
 
@@ -15,6 +15,7 @@ class Shoe
 
   def brand=(brand)
     @brand = brand
-    BRANDS << "Uggs"
+    BRANDS << brand
   end
 end
+
